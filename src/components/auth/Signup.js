e439c4399bf977f0c570a8f1752a23b447064d37
MyNa Do <myna.do@mahoyo.com>
2021-01-04 17:17:13 +0100

    boilerplate for signup component

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
+import { useAuth } from '../../contexts/AuthContext'
 
 const Signup = () => {
+	const { signup } = useAuth()
 	return (
-		<div>
-			Sign up component
-		</div>
+		<Container>
+			<Form>
+				<Form.Group>
+					<Form.Label>Email</Form.Label>
+					<Form.Control type="email" placeholder="Enter email" />
+				</Form.Group>
+
+				<Form.Group>
+					<Form.Label>Password</Form.Label>
+					<Form.Control type="password" placeholder="Password" />
+				</Form.Group>
+
+				<Form.Group>
+					<Form.Label>Password Confirmation</Form.Label>
+					<Form.Control type="password" placeholder="Password" />
+				</Form.Group>
+
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+		</Container>
 	)
 }
 
