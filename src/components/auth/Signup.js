6414e32722d20e05c9fd9d9987c1ede6299aae4a
MyNa Do <myna.do@mahoyo.com>
2021-01-04 17:49:52 +0100

    Sign up new user works

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,29 +1,75 @@
-import React from 'react'
-import { Container, Form, Button } from 'react-bootstrap'
+import React, { useRef, useState } from 'react'
+import { Container, Form, Button, Alert } from 'react-bootstrap'
 import { useAuth } from '../../contexts/AuthContext'
 
 const Signup = () => {
 	const { signup } = useAuth()
+	const emailRef = useRef()
+	const passwordRef = useRef()
+	const passwordConfirmRef = useRef()
+	const [error, setError] = useState(null)
+	const [loading, setLoading] = useState(false)
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		console.log(loading)
+		if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+			return setError("The passwords doesn't match")
+		}
+		setError(null)
+		
+
+		try {
+			setLoading(true)
+			console.log(loading)
+			await signup(emailRef.current.value, passwordRef.current.value)
+		} catch (e) {
+			setError(e.message)
+			setLoading(false)
+			console.log(loading)
+		}
+	}
 	return (
 		<Container>
-			<Form>
+			<h1>Sign Up</h1>
+			{error && (
+				<Alert variant="warning">{error}</Alert>
+			)}
+			<Form onSubmit={handleSubmit}>
 				<Form.Group>
 					<Form.Label>Email</Form.Label>
-					<Form.Control type="email" placeholder="Enter email" />
+					<Form.Control
+						type="email"
+						placeholder="Enter email"
+						ref={emailRef}
+						required
+					/>
 				</Form.Group>
 
 				<Form.Group>
 					<Form.Label>Password</Form.Label>
-					<Form.Control type="password" placeholder="Password" />
+					<Form.Control
+						type="password"
+						placeholder="Password"
+						ref={passwordRef}
+						required
+						autoComplete="off"
+					/>
 				</Form.Group>
 
 				<Form.Group>
 					<Form.Label>Password Confirmation</Form.Label>
-					<Form.Control type="password" placeholder="Password" />
+					<Form.Control
+						type="password"
+						placeholder="Password"
+						ref={passwordConfirmRef}
+						required
+						autoComplete="off"
+					/>
 				</Form.Group>
 
-				<Button variant="primary" type="submit">
-					Submit
+				<Button disabled={loading} variant="primary" type="submit">
+					Create an account
 				</Button>
 			</Form>
 		</Container>
