5fc64689431c71ded47235cf7eec10d2d679011f
MyNa Do <myna.do@mahoyo.com>
2021-01-04 16:53:01 +0100

    Routing and components for signup, login and home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './assets/scss/App.scss'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home from './components/Home'
 
 const App = () => {
 	return (
-		<div>
-			<h1>Photo Review App</h1>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/">
+					<Home />
+				</Route>
+				
+				<Route path="/signup">
+					<Signup />
+				</Route>
+
+				<Route path="/login">
+					<Login />
+				</Route>
+
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
