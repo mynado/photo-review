27f7d3341d2c7cd2021df621f9cac497a7d63746
MyNa Do <myna.do@mahoyo.com>
2021-01-05 18:38:23 +0100

    cleanup

--- src/components/albums/Album.js
+++ src/components/albums/Album.js
@@ -3,12 +3,10 @@ import { useParams } from 'react-router-dom'
 import useAlbum from '../../hooks/useAlbum'
 import Images from '../images/Images'
 import ImageUpload from '../images/ImageUpload'
-import { useAuth } from '../../contexts/AuthContext'
 import useImages from '../../hooks/useImages'
 
 const Album = () => {
 	const { albumId } = useParams()
-	// const { album, images, loading } = useAlbum(albumId)
 	const { album, loading } = useAlbum(albumId)
 	const { images, imgLoading } = useImages(albumId)
 
