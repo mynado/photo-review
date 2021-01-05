6fb5ad49f22653e6a3971202391853794b7da4a2
MyNa Do <myna.do@mahoyo.com>
2021-01-05 18:49:51 +0100

    get all albums from firestore and show in albums component

--- src/components/albums/Albums.js
+++ src/components/albums/Albums.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import useAlbums from '../../hooks/useAlbums'
+import { Link } from 'react-router-dom'
 
 const Albums = () => {
-	
+	const { albums, loading } = useAlbums()
+
 	return (
 		<div>
-			Your albums
+			<h1>All Albums</h1>
+			{
+				loading
+					? (<p>Loading...</p>)
+					: (<ul>
+						{albums.map(album => (
+							<li key={album.id}><Link to={`/albums/${album.id}`}>{album.title}</Link></li>
+						))}
+					</ul>)
+			}
 		</div>
 	)
 }
