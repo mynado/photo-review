5889344f56c339f710bda70b74d2667ef5080946
MyNa Do <myna.do@mahoyo.com>
2021-01-07 15:14:51 +0100

    styling for albums component

--- src/components/albums/Albums.js
+++ src/components/albums/Albums.js
@@ -15,7 +15,7 @@ const Albums = () => {
 	return (
 		<div>
 			<div className="d-flex justify-content-between mt-3 mb-3">
-				<h2>Your albums</h2>
+				<h2>Hello {currentUser.displayName}</h2>
 				<Link to="/albums/create">
 					<Button variant="outline-dark">+ Album</Button>
 				</Link>
@@ -27,26 +27,29 @@ const Albums = () => {
 						<Row className="album-list">
 						{
 							albums.map(album => (
-								<Col xs={12} sm={6} md={4} lg={3} key={album.id}>
-									<Card className={`mb-3 ${album.selection === 'guest' ? 'album-list-guest' : 'album-list-you'}`}>
-										<Card.Body className="album-list-card">
-											<Card.Title className="album-list-title">
-												<Link to={`/albums/${album.id}`}>{album.title}</Link>
-											</Card.Title>
-											<Card.Subtitle>Created by {album.selection}</Card.Subtitle>
-										</Card.Body>
-										<Card.Footer className="text-muted text-right mt-2">
-											<Link 
-												to={`/albums/${album.id}/edit`}
-												className="mt-1 mr-1 btn btn-outline-dark btn-sm"
-												><AiFillEdit />
-											</Link>
-											<Button 
-												variant="outline-danger"
-												size="sm"
-												className="mt-1"
-												onClick={() => handleDeleteAlbum(album)}><IoTrashBin />
-											</Button>
+								<Col xs={12} sm={6} md={6} lg={4} key={album.id} className="col-padding">
+									<Card className="mb-3 album-list-card">
+										<Link to={`/albums/${album.id}`}>
+											<Card.Img variant="top" src={album.img_url} />
+										</Link>
+										<Card.Footer className="text-muted d-flex justify-content-between align-items-center">
+											<div className="d-flex flex-column">
+												<Link className="album-list-title" to={`/albums/${album.id}`}>{album.title}</Link>
+												<small className="album-list-date"> By {album.created_by} {album.date}</small>
+											</div>
+											<div className="button-wrapper">
+												<Link 
+													to={`/albums/${album.id}/edit`}
+													className="mt-1 mr-1 btn btn-light btn-sm button-round"
+													><AiFillEdit />
+												</Link>
+												<Button 
+													variant="light"
+													size="sm"
+													className="mt-1 button-round"
+													onClick={() => handleDeleteAlbum(album)}><IoTrashBin />
+												</Button>
+											</div>
 										</Card.Footer>
 									</Card>
 								</Col>
