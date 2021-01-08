c40b936e4ac8112adce8ac9f4bf149641df56190
MyNa Do <myna.do@mahoyo.com>
2021-01-08 11:11:33 +0100

    moved checkbox styles to own scss-file

--- src/components/images/Checkbox.js
+++ src/components/images/Checkbox.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { ImCheckboxChecked, ImCheckboxUnchecked } from 'react-icons/im'
 import { useImage } from '../../contexts/ImageContext'
+import './Checkbox.scss'
 
 const Checkbox = ({image}) => {
     const [isChecked, setIsChecked] = useState(false)
