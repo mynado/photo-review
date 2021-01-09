d96101a68552ce51aa57934ebef5c4bbaf7d7ad5
MyNa Do <myna.do@mahoyo.com>
2021-01-09 16:17:39 +0100

    album update layout

--- src/components/albums/AlbumUpdate.js
+++ src/components/albums/AlbumUpdate.js
@@ -1,6 +1,6 @@
 import { useState, useRef } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Alert, Button, Form } from 'react-bootstrap'
+import { Alert, Form, Row, Col } from 'react-bootstrap'
 import { db } from '../../firebase'
 import useAlbum from '../../hooks/useAlbum'
 
@@ -31,28 +31,30 @@ const AlbumUpdate = () => {
 	}
 
 	return (
-			<>
-			<h1>Edit Album</h1>
-			{error && (
-				<Alert variant="warning">{error}</Alert>
-			)}
-			<Form onSubmit={handleSubmit}>
-				<Form.Group>
-					<Form.Label>Title</Form.Label>
-					<Form.Control
-						type="text"
-						placeholder="Enter name"
-						ref={titleRef}
-						defaultValue={album.title}
-						required
-					/>
-				</Form.Group>
+		<Row className="justify-content-md-center">
+			<Col xs={12} md={6} lg={4}>
+				<h1>Edit Album</h1>
+				{error && (
+					<Alert variant="warning">{error}</Alert>
+				)}
+				<Form onSubmit={handleSubmit}>
+					<Form.Group>
+						<Form.Label>Title</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="Enter name"
+							ref={titleRef}
+							defaultValue={album.title}
+							required
+						/>
+					</Form.Group>
 
-				<Button variant="primary" type="submit">
-					Update
-				</Button>
-			</Form>
-		</>
+					<button className="custom-btn btn-100" type="submit">
+						Update
+					</button>
+				</Form>
+			</Col>
+		</Row>
 	)
 }
 
