21b6ecfe353e77b6b9b4013a955cdcdf8cdbf3a1
MyNa Do <myna.do@mahoyo.com>
2021-01-09 12:54:09 +0100

    checkmark on photos selected by guest, and button styling

--- src/components/albums/Albums.js
+++ src/components/albums/Albums.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Button, Row, Col, Card } from 'react-bootstrap'
+import { Row, Col, Card } from 'react-bootstrap'
 import './Albums.scss'
 import { IoTrashBin } from 'react-icons/io5'
 import { AiFillEdit } from 'react-icons/ai'
+import { FaCheck } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
 import { useImage } from '../../contexts/ImageContext'
@@ -15,10 +16,10 @@ const Albums = () => {
 
 	return (
 		<div>
-			<div className="d-flex justify-content-between mt-3 mb-3">
+			<div className="d-flex justify-content-between mt-3 mb-3 align-items-center">
 				<h2>Hello {currentUser.displayName}</h2>
 				<Link to="/albums/create">
-					<Button variant="outline-dark">+ Album</Button>
+					<button className="custom-btn">+ Album</button>
 				</Link>
 			</div>
 			{
@@ -30,6 +31,11 @@ const Albums = () => {
 							albums.map(album => (
 								<Col xs={12} sm={6} md={6} lg={4} key={album.id} className="col-padding">
 									<Card className="mb-3 album-list-card">
+										{
+											album.created_by === 'guest'
+												? <span className="album-list-card-guest"><FaCheck /></span>
+												: ('')
+										}
 										<Link to={`/albums/${album.id}`}>
 											<Card.Img variant="top" src={album.img_url} width="100%" height="300px" />
 										</Link>
@@ -39,17 +45,15 @@ const Albums = () => {
 												<small className="album-list-date"> By {album.created_by} {album.date}</small>
 											</div>
 											<div className="button-wrapper">
-												<Link 
-													to={`/albums/${album.id}/edit`}
-													className="mt-1 mr-1 btn btn-light btn-sm button-round"
-													><AiFillEdit />
+												<Link to={`/albums/${album.id}/edit`}>
+													<button className="mt-1 mr-1 custom-btn button-round">
+														<AiFillEdit />
+													</button>
 												</Link>
-												<Button 
-													variant="light"
-													size="sm"
-													className="mt-1 button-round"
+												<button
+													className="mt-1 button-round custom-btn"
 													onClick={() => handleDeleteAlbum(album)}><IoTrashBin />
-												</Button>
+												</button>
 											</div>
 										</Card.Footer>
 									</Card>
