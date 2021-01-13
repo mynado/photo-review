6d4bb3c5b2df4500df956c9c6b3149da024c4519
MyNa Do <myna.do@mahoyo.com>
2021-01-13 11:12:54 +0100

    new album card ui

--- src/components/albums/AlbumCard/AlbumCard.js
+++ src/components/albums/AlbumCard/AlbumCard.js
@@ -2,17 +2,25 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Col, Card } from 'react-bootstrap'
 import { IoTrashBin } from 'react-icons/io5'
-import { AiFillEdit, AiFillLike } from 'react-icons/ai'
+import { AiFillLike } from 'react-icons/ai'
+import { HiDotsHorizontal } from 'react-icons/hi'
 import { useAlbumContext } from '../../../contexts/AlbumContext'
 import noImage from '../../../assets/images/no-image.jpg'
+import './AlbumCard.scss'
+import AlbumUpdate from '../AlbumUpdate/AlbumUpdate'
 
 const AlbumCard = ({ album }) => {
 	const [imgUrl, setImgUrl] = useState()
+	const [showEdit, setShowEdit] = useState(false)
 	const { handleDeleteAlbum } = useAlbumContext()
 
 	useEffect(() => {
 		setImgUrl(album.img_url)
 	}, [album.img_url])
+
+	const handleShowEdit = () => {
+		setShowEdit(!showEdit)
+	}
 	
 	return (
 		<Col xs={12} sm={6} md={6} lg={4} key={album.id} className="col-padding">
@@ -22,24 +30,27 @@ const AlbumCard = ({ album }) => {
 						? <span className="album-list-card-guest"><AiFillLike /></span>
 						: ('')
 				}
+				<button className="ml-auto btn-unstyled" onClick={handleShowEdit}>
+					<HiDotsHorizontal />
+				</button>
 				<Link to={`/albums/${album.id}`}>
 					<Card.Img variant="top" src={imgUrl} onError={() => setImgUrl(noImage)} width="100%" height="300px" />
 				</Link>
-				<Card.Footer className="text-muted d-flex justify-content-between align-items-center">
+				<Card.Footer className="text-muted d-flex justify-content-between">
 					<div className="d-flex flex-column">
-						<Link className="album-list-title" to={`/albums/${album.id}`}>{album.title}</Link>
+						{
+							showEdit
+								? (
+									<>
+										<AlbumUpdate albumId={album.id}/>
+									</>
+								)
+								: (<Link className="album-list-title" to={`/albums/${album.id}`}>{album.title}</Link>)
+						}
 						<small className="album-list-date"> By {album.created_by} {album.date}</small>
 					</div>
 					<div className="button-wrapper">
-						<Link to={`/albums/${album.id}/edit`}>
-							<button className="mt-1 mr-1 custom-btn button-round">
-								<AiFillEdit />
-							</button>
-						</Link>
-						<button
-							className="mt-1 button-round custom-btn"
-							onClick={() => handleDeleteAlbum(album)}><IoTrashBin />
-						</button>
+						{ showEdit &&(<button className="btn-unstyled" onClick={() => handleDeleteAlbum(album)}><IoTrashBin /> </button>) }	
 					</div>
 				</Card.Footer>
 			</Card>
--- src/components/albums/AlbumUpdate/AlbumUpdate.js
+++ src/components/albums/AlbumUpdate/AlbumUpdate.js
@@ -1,62 +1,41 @@
-import { useState, useRef } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { Alert, Form, Row, Col } from 'react-bootstrap'
-import { IoChevronBack } from 'react-icons/io5'
+import { useRef } from 'react'
+import { Form} from 'react-bootstrap'
 import { db } from '../../../firebase'
 import useAlbum from '../../../hooks/useAlbum'
+import './AlbumUpdate.scss'
 
-const AlbumUpdate = () => {
-	const { albumId } = useParams()
+const AlbumUpdate = ({ albumId }) => {
 	const titleRef = useRef()
-	const [error, setError] = useState(null)
-	const navigate = useNavigate()
 	const { album, loading } = useAlbum(albumId)
 
 	if (loading) {
-		return (<p>Loading...</p>)
+		return ('')
 	}
 
 	const handleSubmit = async (e) => {
 		e.preventDefault()
-		setError(null)
 
 		try {
 			await db.collection('albums').doc(albumId).update({
 				title: titleRef.current.value,
 			})
-
-			navigate(`/albums/${albumId}`)
 		} catch (e) {
-			setError(e.message)
 		}
 	}
 
 	return (
-		<Row className="justify-content-md-center">
-			<Col xs={12} md={6} lg={4}>
-				<h1>Edit Album</h1>
-				{error && (
-					<Alert variant="warning">{error}</Alert>
-				)}
-				<Form onSubmit={handleSubmit} className="mb-4">
-					<Form.Group>
-						<Form.Label>Title</Form.Label>
-						<Form.Control
-							type="text"
-							placeholder="Enter name"
-							ref={titleRef}
-							defaultValue={album.title}
-							required
-						/>
-					</Form.Group>
-
-					<button className="custom-btn btn-100" type="submit">
-						Update
-					</button>
-				</Form>
-				<button onClick={() => navigate(-1)} className="custom-btn"><IoChevronBack />Back</button>
-			</Col>
-		</Row>
+			<Form onSubmit={handleSubmit} className="card-title-form">
+				<Form.Group className="card-title-form-group">
+					<Form.Control
+						className="card-title-form-input"
+						type="text"
+						placeholder="Enter name"
+						ref={titleRef}
+						defaultValue={album.title}
+						required
+					/>
+				</Form.Group>
+			</Form>
 	)
 }
 
