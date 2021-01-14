73bf035eee24652dae3c8aa1f18bb43724659d60
MyNa Do <myna.do@mahoyo.com>
2021-01-14 11:24:58 +0100

    new styles for photo review cards

--- src/components/images/buttons/LikeDislikeButtons.js
+++ src/components/images/buttons/LikeDislikeButtons.js
@@ -1,25 +1,27 @@
 import React from 'react'
-import { AiFillLike, AiFillDislike, AiOutlineLike, AiOutlineDislike } from 'react-icons/ai'
+import { AiFillDislike, AiOutlineDislike } from 'react-icons/ai'
+import { FcLike, FcLikePlaceholder } from 'react-icons/fc'
 import { useImageContext } from '../../../contexts/ImageContext'
+import './LikeDislikeButtons.scss'
 
 const Checkbox = ({image}) => {
 	const { handleLikeImage, handleDislikeImage, imageToAdd, imageToDelete } = useImageContext()
 
 	return (
 		<>
-			<div className="image-item-rate-buttons">
-				<button className={`custom-btn rate-btn ${imageToAdd.includes(image) ? 'rate-btn-selected-like' : ''}`} onClick={() => handleLikeImage(image)}>
+			<div className="rate-btn-wrapper">
+				<button className="btn-unstyled rate-btn mr-1" onClick={() => handleLikeImage(image)}>
 					{
 						imageToAdd.includes(image)
-							? <AiFillLike className="rate-btn-like" />
-							: <AiOutlineLike />
+							? <FcLike className="rate-btn-like" />
+							: <FcLikePlaceholder className="rate-btn-like rate-btn-like-placeholder" />
 					}
 				</button>
-				<button className={`custom-btn rate-btn ${imageToDelete.includes(image) ? 'rate-btn-selected-dislike' : ''}`}  onClick={() => handleDislikeImage(image)}>
+				<button className="btn-unstyled rate-btn"  onClick={() => handleDislikeImage(image)}>
 					{
 						imageToDelete.includes(image)
 							? <AiFillDislike className="rate-btn-dislike" />
-							: <AiOutlineDislike />
+							: <AiOutlineDislike className="rate-btn-dislike rate-btn-dislike-placeholder" />
 					}
 				</button>
 			</div>
--- src/components/images/image/Image.js
+++ src/components/images/image/Image.js
@@ -5,6 +5,7 @@ import { useAuth } from '../../../contexts/AuthContext'
 import { useImageContext } from '../../../contexts/ImageContext'
 import Checkbox from '../buttons/Checkbox'
 import LikeDislikeButtons from '../buttons/LikeDislikeButtons'
+import './Image.scss'
 
 const Image = ({ showedit, image }) => {
 	const { handleDeleteImage } = useImageContext()
@@ -13,22 +14,46 @@ const Image = ({ showedit, image }) => {
 
 	return (
 		<Col xs={12} sm={6} md={6} lg={4} className="col-padding image-item">
-			<img className="img-fluid" src={image.url} alt="" />
-				{
-					currentUser 
-						? showedit 
-							? (
-								<div className="image-item-edit-buttons d-flex flex-column align-items-center">
-									<Checkbox image={image} />
-									<button className="custom-btn image-item-trash-button" onClick={() => handleDeleteImage(image)}>
-										<IoTrashBin className="trash-icon" />
-									</button>
-								</div>
-							) : ('')
-						: (
+			{
+				currentUser
+					? (
+						<div className="image-item-wrapper-auth">
+							<img className="img-fluid image-item-img" src={image.url} alt="" />
+								{showedit &&(
+									<div className="image-item-edit-buttons d-flex flex-column align-items-center">
+										<Checkbox image={image} />
+										<button className="custom-btn image-item-trash-button" onClick={() => handleDeleteImage(image)}>
+											<IoTrashBin className="trash-icon" />
+										</button>
+									</div>
+								)}
+						</div>
+					)
+					: (
+						<div className="image-item-wrapper-guest">
+							<img className="img-fluid image-item-img" src={image.url} alt="" />
 							<LikeDislikeButtons image={image}/>
-						)
-				}
+						</div>
+					)
+			}
+			{/* <div className="p-5">
+				<img className="img-fluid" src={image.url} alt="" />
+					{
+						currentUser 
+							? showedit 
+								? (
+									<div className="image-item-edit-buttons d-flex flex-column align-items-center">
+										<Checkbox image={image} />
+										<button className="custom-btn image-item-trash-button" onClick={() => handleDeleteImage(image)}>
+											<IoTrashBin className="trash-icon" />
+										</button>
+									</div>
+								) : ('')
+							: (
+								<LikeDislikeButtons image={image}/>
+							)
+					}
+				</div> */}
 		</Col>
 	)
 }
